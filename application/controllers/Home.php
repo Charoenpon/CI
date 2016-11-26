b00aab8d71a0b7f65f18ec4eee1cbd04b4d0c004
Charoenpon <Charoenpon.nel@gmail.com>
2016-11-26 14:23:20 +0700

    Add htaccess

---
+++ application/controllers/Home.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 26/11/2559
+ * Time: 13:22
+ */
+class Home extends CI_Controller
+{
+    public function index(){
+        echo "Hello";
+    }
+ /**   public function test($name = "Mr.Charoenpon", $lastname ="Inpunkeaw"){
+        echo "My name is :".$name ." ". $lastname;
+    }
+
+    public function calculator($num1 , $num2)
+    {
+        echo $num1 + $num2;
+    }
+  */
+}
