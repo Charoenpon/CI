147b30c38e9484155338730c446955e98f48572d
Charoenpon <Charoenpon.nel@gmail.com>
2016-11-27 12:07:39 +0700

    Edit layout_admin และแยกส่วน header footer

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -2,14 +2,17 @@
 
 /**
  * Created by PhpStorm.
- * User: User
+ * User: lab
  * Date: 27/11/2559
- * Time: 11:04
+ * Time: 10:45
  */
 class Admin extends CI_Controller
 {
     public function index()
     {
+        $this->load->view('admin/layout/admin_header_view');
+        $this->load->view('admin/layout/admin_sidebar_view');
         $this->load->view('admin/admin_view');
+        $this->load->view('admin/layout/admin_footer_view');
     }
 }
